d9bc288e7e365f4e7a9f0a180c9f399b58019663
rizkyy20 <rizkyfile12@gmail.com>
2026-01-25 21:12:45 +0800

    Membuat halaman admin

--- index.php
+++ index.php
@@ -16,32 +16,12 @@ $query = mysqli_query($conn, "
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <title>Home</title>
 </head>
 <body>
     <h1>Halaman Home</h1>
 
-    <table border="1">
-        <thead>
-            <tr>
-                <td>NIM</td>
-                <td>NAMA</td>
-                <td>JURUSAN</td>
-                <td>ALAMAT</td>
-            </tr>
-        </thead>
-        <tbody>
-            <?php while($row = mysqli_fetch_assoc($query)) {
-                    ?>
-            <tr> 
-                <td><?= $row['nim'] ?></td>
-                <td><?= $row['nama'] ?></td>
-                <td><?= $row['nama_jurusan'] ?></td>
-                <td><?= $row['alamat'] ?></td>
-            </tr>
-                <?php }?>
-        </tbody>
-    </table>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
